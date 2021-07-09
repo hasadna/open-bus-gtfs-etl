86346dd0ff4e16437e62614cee6b4e7fa1345bc6
Aviv Sela <avivsela10+github@gmail.com>
2021-07-09 13:09:24 +0300

    add simple gtfs extractor from ftp

---
+++ gtfs_extractor/gtfs_extractor.py
@@ -0,0 +1,61 @@
+import shutil
+from contextlib import closing
+from pathlib import Path
+from typing import Dict
+from urllib import request
+
+from pydantic import BaseModel
+from tqdm import tqdm
+
+
+__CONFIGURATION_FILE_NAME = "gtfs_extractor.config"
+
+
+class FileConfig(BaseModel):
+    url: str
+    local_name: str
+
+
+class GtfsExtractorConfig(BaseModel):
+    gtfs_file: FileConfig
+    tariff_file: FileConfig
+    cluster_file: FileConfig
+    trip_id_to_date_file: FileConfig
+
+
+GTFS_EXTRACTOR_CONFIG = GtfsExtractorConfig.parse_file(Path(__file__).parent.joinpath(__CONFIGURATION_FILE_NAME))
+
+
+class GTFSFiles(BaseModel):
+    gtfs: Path
+    tariff: Path
+    cluster_to_line: Path
+    trip_id_to_date: Path
+
+
+class GtfsRetriever:
+    def __init__(self, folder: str, app_config: GtfsExtractorConfig = GTFS_EXTRACTOR_CONFIG):
+        self.folder = folder
+        self.app_config = app_config
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def retrieve_gtfs_files(self):
+        args: Dict[str, FileConfig] = dict(gtfs=self.app_config.gtfs_file, tariff=self.app_config.tariff_file,
+                                           cluster_to_line=self.app_config.cluster_file,
+                                           trip_id_to_date=self.app_config.trip_id_to_date_file)
+
+        local_paths = GTFSFiles(**{k: Path(self.folder, v.local_name) for k, v in args.items()})
+
+        for item in tqdm(args.values()):
+            self.download_file_from_ftp(url=item.url, local_file=Path(self.folder, item.local_name))
+
+        return local_paths
+
+    @staticmethod
+    def download_file_from_ftp(url, local_file: Path):
+        with closing(request.urlopen(url)) as r:
+            with local_file.open('wb') as f:
+                shutil.copyfileobj(r, f)
+
--- test_app.py
+++ test_app.py
@@ -1,8 +1,12 @@
 import datetime
 import os
+import tempfile
 from pathlib import Path
 
+from gtfs_extractor.gtfs_extractor import GtfsRetriever, GtfsExtractorConfig, GTFS_EXTRACTOR_CONFIG
 from gtfs_stat.gtfs_stats import create_trip_and_route_stat
+
+
 
 
 class TestGtgsStat:
@@ -19,5 +23,23 @@ class TestGtgsStat:
         assert tmp_path.joinpath('route_stats.csv.gz').is_file()
         assert tmp_path.joinpath('trip_stats.csv.gz').is_file()
 
+
+class TestGtfsExtractor:
+    def test_retrieve_gtfs_files_without_download(self, tmp_path: Path):
+        tmp_dir = tmp_path.as_posix()
+
+        retriever = GtfsRetriever(folder=tmp_dir)
+        retriever.download_file_from_ftp = lambda url, local_file: None
+        actual = retriever.retrieve_gtfs_files()
+
+        assert actual.gtfs == Path(tmp_dir, GTFS_EXTRACTOR_CONFIG.gtfs_file.local_name)
+        assert actual.tariff == Path(tmp_dir, GTFS_EXTRACTOR_CONFIG.tariff_file.local_name)
+        assert actual.cluster_to_line == Path(tmp_dir, GTFS_EXTRACTOR_CONFIG.cluster_file.local_name)
+        assert actual.trip_id_to_date == Path(tmp_dir, GTFS_EXTRACTOR_CONFIG.trip_id_to_date_file.local_name)
+
+    def test_config(self, tmp_path: Path):
+        local_file = tmp_path.joinpath('test.zip')
+        GtfsRetriever.download_file_from_ftp(GTFS_EXTRACTOR_CONFIG.tariff_file.url, local_file)
+        assert os.path.isfile(local_file.as_posix())
 
 
